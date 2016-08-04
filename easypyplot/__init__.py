43491f75d8e539b2b0e0383f5f41d4c748456985
Mingyu Gao <mgao12@stanford.edu>
2016-08-04 14:30:04 -0700

    Lint code.

--- easypyplot/__init__.py
+++ easypyplot/__init__.py
@@ -4,10 +4,10 @@
  *
 """
 
-__version__ = '0.1.0'
-
 import easypyplot.barchart
 import easypyplot.color
 import easypyplot.format
 import easypyplot.pdf
+
+__version__ = '0.1.0'
 
--- easypyplot/barchart.py
+++ easypyplot/barchart.py
@@ -100,7 +100,7 @@ def draw(axes,
         if len(hatchs) != num_entries:
             raise ValueError('[barchart] Given hatchs do not match the data')
     else:
-        hatchs=[ None for eid in range(num_entries) ]
+        hatchs = [None for eid in range(num_entries)]
 
 
     ############################################################################
