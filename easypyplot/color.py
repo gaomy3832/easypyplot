d5d0ec898ead94e67b567e6939dd1f8bd02975e8
Mingyu Gao <mgao12@stanford.edu>
2017-08-23 21:31:16 -0700

    Use colormap for color scale.

--- easypyplot/color.py
+++ easypyplot/color.py
@@ -6,23 +6,32 @@
 import numpy as np
 import matplotlib.colors
 
-
 # Colors from http://www.colorbrewer2.org/, 8-class, qualitative, Accent
 COLOR_SET = ['#386cb0', '#7fc97f', '#f0027f', '#beaed4', \
              '#bf5b17', '#fdc086', '#666666', '#ffff99']
 
-def color_scale(refhex, num, low=0.4, high=0.9):
+def color_scale(ref, num, low=0.2, high=0.9):
     """ Get a color scale from shallow to dark.
 
-    refhex: referenced color in HEX format.
+    ref: referenced color.
     num: number of colors in the scale.
     low: low brightness.
     high: high brightness.
     """
-    refc = matplotlib.colors.hex2color(refhex)
-    maxrefc = max(refc)
-    refc = [x / maxrefc for x in refc]
-    colors = [matplotlib.colors.rgb2hex([x * s for x in refc])
-              for s in np.linspace(low, high, num)]
+    # Saturate the color.
+    rgb = matplotlib.colors.to_rgb(ref)
+    if max(rgb) < 1e-4:
+        # All 0, black.
+        rgb = (1.,) * len(rgb)
+    else:
+        rgb = tuple(v / max(rgb) for v in rgb)
+
+    # Make a colormap.
+    cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+        'cm', [(0, 0, 0), rgb])
+
+    # Linearly select the colors.
+    colors = [cmap(x) for x in np.linspace(low, high, num)]
+
     return colors
 
