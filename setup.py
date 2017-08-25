0092d35d56d203c49dde8ad10a5b6706748ada7d
Mingyu Gao <mgao12@stanford.edu>
2017-08-25 15:37:40 -0700

    Update setup.py.

--- setup.py
+++ setup.py
@@ -5,38 +5,57 @@
 """
 import os
 import re
-# To use a consistent encoding
-from codecs import open
-# Always prefer setuptools over distutils
 import setuptools
 
-here = os.path.abspath(os.path.dirname(__file__))
+PACKAGE = 'easypyplot'
+DESC = 'Lightweight utilities and wrappers for Python matplotlib.'
 
-package = 'easypyplot'
-version = '0.0.0'
-desc = 'Python matplotlib utilities and wrappers'
+def _get_version():
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, PACKAGE, '__init__.py'), 'r') as fh:
+        matches = re.findall(r'^\s*__version__\s*=\s*[\'"]([^\'"]+)[\'"]',
+                             fh.read(), re.M)
+        if matches:
+            return matches[-1]
+    return '0.0.0'
 
-# Get version number
-with open(os.path.join(here, package, '__init__.py'), encoding='utf-8') as fh:
-    matches = re.findall(r'^\s*__version__\s*=\s*[\'"]([^\'"]+)[\'"]',
-            fh.read(), re.M)
-    if matches:
-        version = matches[-1]
+def _readme():
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, 'README.rst'), 'r') as fh:
+        return fh.read()
 
 setuptools.setup(
-    name=package,
-    version=version,
-
-    description=desc,
+    name=PACKAGE,
+    version=_get_version(),
+    description=DESC,
 
     author='Mingyu Gao',
     author_email='mgao12@stanford.edu',
+    long_description=_readme(),
+    url='https://github.com/gaomy3832/easypyplot',
+    license='BSD 3-clause',
 
-    #long_description='',
-    #url='',
-    #license='',
+    packages=setuptools.find_packages(),
 
-    packages=[package],
+    install_requires=[
+        'coverage>=4',
+        'matplotlib>=1.3',
+        'numpy>=1.8',
+        'nose>=1.3',
+        'cycler>=0.10',
+    ],
 
-    #install_requires=[],
+    keywords='matplotlib python-plot',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Artistic Software',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Utilities',
+    ],
 )
+
