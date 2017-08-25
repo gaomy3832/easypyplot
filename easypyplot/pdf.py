be16b3c2b67f160e46ed951a8e9e691bc09b8d05
Mingyu Gao <mgao12@stanford.edu>
2017-08-25 10:29:33 -0700

    Add a context manager for PDF plot.

--- easypyplot/pdf.py
+++ easypyplot/pdf.py
@@ -3,22 +3,23 @@
  * All rights reserved.
  *
 """
+from contextlib import contextmanager
 import matplotlib.backends.backend_pdf
+
 from .format import paper_plot
 
-
-def plot_setup(name, dims, fontsize=9):
+def plot_setup(name, figsize=None, fontsize=9):
     """ Setup a PDF page for plot.
 
     name: PDF file name. If not ending with .pdf, will automatically append.
-    dims: dimension of the plot in inches, should be an array of length two.
+    figsize: dimension of the plot in inches, should be an array of length two.
     fontsize: fontsize for legends and labels.
     """
     paper_plot(fontsize)
     if not name.endswith('.pdf'):
         name += '.pdf'
     pdfpage = matplotlib.backends.backend_pdf.PdfPages(name)
-    fig = matplotlib.pyplot.figure(figsize=dims)
+    fig = matplotlib.pyplot.figure(figsize=figsize)
     return pdfpage, fig
 
 
@@ -30,4 +31,16 @@ def plot_teardown(pdfpage):
     pdfpage.savefig()
     pdfpage.close()
 
+
+@contextmanager
+def plot_open(name, figsize=None, fontsize=9):
+    """ Open a context of PDF page for plot, used for the `with` statement.
+
+    name: PDF file name. If not ending with .pdf, will automatically append.
+    figsize: dimension of the plot in inches, should be an array of length two.
+    fontsize: fontsize for legends and labels.
+    """
+    pdfpage, fig = plot_setup(name, figsize=figsize, fontsize=fontsize)
+    yield fig
+    plot_teardown(pdfpage)
 
