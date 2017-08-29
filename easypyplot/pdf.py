deda5739de20fe52488e7c2c2f7922eca5b404a5
Mingyu Gao <mgao12@stanford.edu>
2017-08-29 10:06:25 -0700

    Inject license header.

--- easypyplot/pdf.py
+++ easypyplot/pdf.py
@@ -1,8 +1,19 @@
+""" $lic$
+Copyright (c) 2017, Mingyu Gao
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the Modified BSD-3 License as published by the Open Source
+Initiative.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the BSD-3 License for more details.
+
+You should have received a copy of the Modified BSD-3 License along with this
+program. If not, see <https://opensource.org/licenses/BSD-3-Clause>.
 """
- * Copyright (c) 2016. Mingyu Gao
- * All rights reserved.
- *
-"""
+
 from contextlib import contextmanager
 import matplotlib.backends.backend_pdf
 
