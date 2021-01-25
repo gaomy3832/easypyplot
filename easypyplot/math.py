38ee09caecd20cb4f8bfff3f22a93e2dd9845d09
Mingyu Gao <gaomy@tsinghua.edu.cn>
2021-01-25 15:23:18 +0800

    Add math module, including normalization and geomean.

---
+++ easypyplot/math.py
@@ -0,0 +1,91 @@
+""" $lic$
+Copyright (c) 2016-2021, Mingyu Gao
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the Modified BSD-3 License as published by the Open Source
+Initiative.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the BSD-3 License for more details.
+
+You should have received a copy of the Modified BSD-3 License along with this
+program. If not, see <https://opensource.org/licenses/BSD-3-Clause>.
+"""
+
+import numpy as np
+
+def _parse_expected_tuple(arg, default=tuple()):
+    """ Parse the argument into an expected tuple.
+
+    The argument can be None (i.e., using the default), a single element (i.e.,
+    a length-1 tuple), or a tuple
+    """
+    try:
+        _ = iter(arg)
+    except TypeError:
+        tpl = tuple(default) if arg is None else (arg,)
+    else:
+        tpl = tuple(arg)
+    return tpl
+
+
+def normalize(data, axis=None, index=None):
+    """ Normalize data on the given axis
+
+    data: a multi-dim array.
+    axis: axis or axes which makes subarrays to be normalized together.
+        E.g., axis=-1 means to normalize the innermost dimension.
+        None (i.e., all axes) or int or tuple of int.
+    index: index or indices of the elements used for normalization along the
+        normalization axis or axes.
+        None or int or tuple of int.
+
+    Example:
+    data = [[.1, .4, .7], [.2, .5, .8], [.3, .6, .9]]
+    normalize(data) = [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    normalize(data, axis=-1) = [[1, 4, 7], [1, 2.5, 4], [1, 2, 3]]
+    normalize(data, axis=0) = [[1, 1, 1], [2, 1.25, 1.143], [3, 1.5, 1.286]]
+    normalize(data, axis=0, index=1) =
+            [[0.5, 0.8, 0.875], [1, 1, 1], [1.5, 1.2, 1.125]]
+    """
+    din = np.asarray(data)
+    ndim = din.ndim
+    shape = din.shape
+
+    # Parse arguments.
+    axes = _parse_expected_tuple(axis, default=range(ndim))
+    indices = _parse_expected_tuple(index)
+    # Pad with zero indices, or ignore unused indices.
+    indices += (0,) * (len(axes) - len(indices))
+
+    # Constrcut the denominator array through an index array.
+    # The denominator array has the same shape as the data array, except the
+    # normalization axes whose dimensions are 1 and elements are the indexed
+    # elements used for normalization that will be broadcast.
+    # The index array selects the elements used for normalization, using the
+    # given indice along the normalization axes, and a full slice along the
+    # other axes.
+    didx = [slice(None) for _ in range(ndim)]
+    sidx = list(shape)  # indexing loses axis, restore through reshape
+    for d, i in zip(axes, indices):
+        didx[d] = i
+        sidx[d] = 1
+    dnorm = din[tuple(didx)].reshape(tuple(sidx))
+
+    return din / dnorm
+
+
+def geomean(data, axis=None):
+    """ Get the geometric mean along the given axis.
+
+    data: a multi-dim array.
+    axis: axis or axes along which the geomean is performed.
+        None (i.e., all axes) or int or tuple of int.
+    """
+    din = np.asarray(data)
+    prod = np.prod(din, axis=axis)
+    num = din.size // prod.size
+    return prod ** (1. / num)
+
---
+++ easypyplot/tests/test_math.py
@@ -0,0 +1,102 @@
+""" $lic$
+Copyright (c) 2016-2021, Mingyu Gao
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the Modified BSD-3 License as published by the Open Source
+Initiative.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the BSD-3 License for more details.
+
+You should have received a copy of the Modified BSD-3 License along with this
+program. If not, see <https://opensource.org/licenses/BSD-3-Clause>.
+"""
+
+import unittest
+import numpy as np
+
+from easypyplot import math as m  # avoid conflict with built-in.
+
+class TestMath(unittest.TestCase):
+    ''' Tests for math module. '''
+
+    def setUp(self):
+        self.data = np.linspace(1, 3 * 2 * 4, num=3 * 2 * 4, endpoint=True).reshape(3, 2, 4) / 10.
+
+    def test_parse_expected_tuple(self):
+        ''' _parse_expected_tuple. '''
+        # pylint: disable=protected-access
+        self.assertTupleEqual((3,), m._parse_expected_tuple(3))
+        self.assertTupleEqual((-1,), m._parse_expected_tuple(-1))
+        self.assertTupleEqual((2, 3, -1,), m._parse_expected_tuple([2, 3, -1]))
+        self.assertTupleEqual(tuple(), m._parse_expected_tuple(None))
+
+    def test_normalize(self):
+        ''' normalize. '''
+        # Normalize all together.
+        result1 = m.normalize(self.data)
+        expect1 = self.data * 10
+        np.testing.assert_allclose(expect1, result1)
+        self.assertTupleEqual(expect1.shape, result1.shape)
+
+        # Normalize along innermost axis of size 4.
+        result2 = m.normalize(self.data, axis=-1)
+        expect2 = self.data / (np.array([[1, 5], [9, 13], [17, 21]]) / 10.)[..., np.newaxis]
+        np.testing.assert_allclose(expect2, result2)
+        self.assertTupleEqual(expect2.shape, result2.shape)
+
+        # Normalize along outermost axis of size 3.
+        result3 = m.normalize(self.data, axis=0)
+        expect3 = self.data / (np.array([range(1, 1+4), range(5, 5+4)]) / 10.)
+        np.testing.assert_allclose(expect3, result3)
+        self.assertTupleEqual(expect3.shape, result3.shape)
+
+        # Normalize along outermost axis of size 3, using the second index.
+        result4 = m.normalize(self.data, axis=0, index=1)
+        expect4 = self.data / (np.array([range(9, 9+4), range(13, 13+4)]) / 10.)
+        np.testing.assert_allclose(expect4, result4)
+        self.assertTupleEqual(expect4.shape, result4.shape)
+
+        # Normalize along two innermost axes of size (2, 4), using the last index.
+        result5 = m.normalize(self.data, axis=(-1, -2), index=(-1, -1))
+        expect5 = self.data / (np.array([8, 16, 24]) / 10.)[..., np.newaxis, np.newaxis]
+        np.testing.assert_allclose(expect5, result5)
+        self.assertTupleEqual(expect5.shape, result5.shape)
+
+        # Normalize along none axis, i.e., each is normalized individually and becomes 1.
+        result6 = m.normalize(self.data, axis=tuple())
+        expect6 = np.ones(self.data.shape)
+        np.testing.assert_allclose(expect6, result6)
+        self.assertTupleEqual(expect6.shape, result6.shape)
+
+    def test_geomean(self):
+        ''' geomean. '''
+        # Geomean along all.
+        result1 = m.geomean(self.data)
+        expect1 = np.prod(self.data.reshape(None)) ** (1. / self.data.size)
+        np.testing.assert_allclose(expect1, result1)
+        self.assertTupleEqual(expect1.shape, result1.shape)
+
+        # Geomean along innermost axis of size 4.
+        result2 = m.geomean(self.data, axis=-1)
+        expect2 = np.array(
+            [np.prod(range(i, i + 4)) ** (1. / 4) for i in range(1, 1 + 24, 4)]).reshape(3, 2) / 10.
+        np.testing.assert_allclose(expect2, result2)
+        self.assertTupleEqual(expect2.shape, result2.shape)
+
+        # Geomean along outermost axis of size 3.
+        result3 = m.geomean(self.data, axis=0)
+        expect3 = np.array(
+            [np.prod([i, i + 8, i + 16]) ** (1. / 3) for i in range(1, 1 + 8)]).reshape(2, 4) / 10.
+        np.testing.assert_allclose(expect3, result3)
+        self.assertTupleEqual(expect3.shape, result3.shape)
+
+        # Geomean along two innermost axes of size (2, 4).
+        result4 = m.geomean(self.data, axis=(-1, -2))
+        expect4 = np.array(
+            [np.prod(range(i, i + 8)) ** (1. / 8) for i in range(1, 1 + 24, 8)]) / 10.
+        np.testing.assert_allclose(expect4, result4)
+        self.assertTupleEqual(expect4.shape, result4.shape)
+
